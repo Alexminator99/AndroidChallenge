d45225664de5dd7487ed548bff74e2975e483b92
Alexminator99 <alexminator1999@gmail.com>
2024-10-27 12:53:53 +0100

    refactor: Add missing endpoint for fetching market

--- app/src/main/java/com/ethermail/androidchallenge/data/CoincapService.kt
+++ app/src/main/java/com/ethermail/androidchallenge/data/CoincapService.kt
@@ -3,15 +3,21 @@ package com.ethermail.androidchallenge.data
 
 import com.ethermail.androidchallenge.data.model.assets.AssetsApiData
 import com.ethermail.androidchallenge.data.model.markets.MarketsApiData
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 const val HOST_COINCAP = "https://api.coincap.io/"
 
 interface CoincapService {
 
     @GET("/v2/assets")
-    suspend fun getAssets(): AssetsApiData
+    suspend fun getAssets(): Response<AssetsApiData>
 
     @GET("/v2/markets")
-    suspend fun getMarkets(): MarketsApiData
+    suspend fun getMarkets(): Response<MarketsApiData>
+
+    @GET("/v2/assets/{id}/markets")
+    suspend fun getAssetHighestMarket(@Path("id") id: String): Response<MarketsApiData>
+
 }
